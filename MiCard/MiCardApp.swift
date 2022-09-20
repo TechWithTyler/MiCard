c1c18fb8150b57152450584f2dcc2bf94a6ef294
TechWithTyler <twsandmds@gmail.com>
2022-09-20 10:59:10 -0700

    Initial Commit

---
+++ MiCard/MiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCardApp.swift
+//  MiCard
+//
+//  Created by Tyler Sheft on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
